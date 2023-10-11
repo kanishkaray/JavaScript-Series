a9838fcdbbf04a3244f24a149f357de45650f31b
kanishkaray <60337346+kanishkaray@users.noreply.github.com>
2023-10-11 11:41:32 +0000

    comparision of datatypes in js

---
+++ 01_basics/04_comparision.js
@@ -0,0 +1,26 @@
+// console.log(2 > 1);
+// console.log(2 >= 1);
+// console.log(2 < 1);
+// console.log(2 == 1);
+// console.log(2 != 1);
+
+
+// console.log("2" > 1);
+// console.log("02" > 1);
+
+/*
+Avoid this type of conversions
+
+console.log(null > 0);
+console.log(null == 0);
+console.log(null >= 0);
+
+console.log(undefined == 0);
+console.log(undefined > 0);
+console.log(undefined < 0);
+
+*/
+
+// === //checks for values as well as datatypes
+
+console.log("2" === 2);
